b473d1abc1449940490a78d16d5e96a0670b3b9a
Guesly  Germain <germain1211@outlook.com>
2017-11-22 15:44:50 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    "Welcome to your app!!!! I BUILT THIS APP! "
+  get '/' do
+    "I started the shotgun server! "
   end
 
 end
